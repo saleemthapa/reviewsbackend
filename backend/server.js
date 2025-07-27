2b815e137654759cb6abb9cc08b1dd47fc1581d8
saleemthapa <stf73@umsystem.edu>
2025-07-27 15:41:59 +0545

    Feat(API): Reviews by menuid

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,13 @@ const menuItemRoutes = require('./routes/menuItems');
 const reviewsRoutes = require('./routes/reviews');
 
 const app = express();
-app.use(cors());
+
+// Configure CORS to allow requests from your frontend domain
+app.use(cors({
+  origin: ['https://www.reviewsbymenu.com', 'http://localhost:3000', 'http://localhost:5173'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/api/restaurants', restaurantRoutes);
